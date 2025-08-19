738f466df9335d4aea6d30f2f233e24eec5a7d02
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-19 08:14:02 +0200

    practice

---
+++ AIworkflow/Practice.py
@@ -0,0 +1,8 @@
+first_name = "Robert"
+last_name = "Stachowski"
+age = 43
+try_to_learn_python = True
+
+print(f"{first_name} {last_name}, {age}, try to learn python: {try_to_learn_python}")
+age = str(age)
+print(first_name + age)
