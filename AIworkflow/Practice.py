00a1f9a94d4a82537ea5e046fb9ec73b700884a4
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-19 08:23:41 +0200

    practice

--- AIworkflow/Practice.py
+++ AIworkflow/Practice.py
@@ -6,3 +6,24 @@ try_to_learn_python = True
 print(f"{first_name} {last_name}, {age}, try to learn python: {try_to_learn_python}")
 age = str(age)
 print(first_name + age)
+print("-------------------------")
+
+x = 15
+y = 4
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+if x > y:
+    print(True)
+else:
+    print(False)
+
+
+x = x + y
+print(x)
+
