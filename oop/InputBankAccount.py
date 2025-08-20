8fd625da42248c97cd8bd61dce492c4cd1df9659
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-20 13:56:03 +0200

    True/False, try/except repaired. Decimal added

--- oop/InputBankAccount.py
+++ oop/InputBankAccount.py
@@ -1,61 +1,68 @@
+from decimal import Decimal, InvalidOperation
+
+
 class BankAccount:
     def __init__(self, owner, balance=0):
         self.owner = owner
         self.__balance = balance
-        
+
     def deposit(self, amount):
         if amount > 0:
-            self.__balance += amount            
+            self.__balance += amount
             return True
         else:
-            return None   
-   
+            return False
+
     def withdraw(self, amount):
-        if amount <= self.__balance:
-            self.__balance -= amount                        
-            return True          
-        else:            
-            return None            
+        if amount <= self.__balance and amount > 0:
+            self.__balance -= amount
+            return True
+        else:
+            return False
 
     def get_balance(self):
         return self.__balance
+
+
+if __name__ == "__main__":
+    account = BankAccount("Jan Nowak", Decimal("1000"))
     
-account = BankAccount("Jan Nowak", 1000)
+    while True:
+        choice = input("Wpisz polecenie (wplata / wyplata / saldo / wyjscie): ").strip().lower()
+        if choice == "wyjscie":
+            break
 
-while True:
-    menu_amount = input("Wpisz polecenie (wplata / wyplata / wyjscie): ")
-    if menu_amount == "wyjscie" :
-        break
+        elif choice == "saldo":
+             print(f"Saldo wynosi: {account.get_balance()}")
 
-    elif menu_amount == "wyplata" :
-        try:        
-            input_amount = float(input(f"Podaj kwotę wypłaty, mniejszą niż saldo( {account.get_balance()} ): "))       
-            result = account.withdraw(input_amount)
-            
+        elif choice == "wyplata":
+            try:
+                input_amount = Decimal(input(f"Podaj kwotę wypłaty, mniejszą niż saldo( {account.get_balance()} ): "))
+                result = account.withdraw(input_amount)
+            except InvalidOperation:
+                print("Wprowadź poprawną liczbę: ")
+
             if result is True:
                 print(f"Saldo po wypłacie wynosi: {account.get_balance()} ")
-                
             else:
                 print("---")
                 print("Poucinam paluszki ;P ")
-                print("---")     
+                print("---")
 
-        except ValueError:
-            print("Wprowadź poprawną liczbę: ")
-
-    elif menu_amount == "wplata":  
-        try:      
-            input_amount = float(input("Podaj kwotę wpłaty : "))
-            result = account.deposit(input_amount)
+            
+        elif choice == "wplata":
+            try:
+                input_amount = Decimal(input("Podaj kwotę wpłaty : "))
+                result = account.deposit(input_amount)
+            except ValueError:
+                print("Wprowadź poprawna liczbę: ")
 
             if result is True:
                 print(f"Saldo po wpłacie: {account.get_balance()}")
-                
             else:
-               print("Nie można wpłacać ujemnych kwot, zera również...")
-        except ValueError:
-            print("Wprowadź poprawna liczbę: ")
+                print("Nie można wpłacać ujemnych kwot, zera również...")
+            
 
-    else: 
-        print("Halo, halo co tu się dzieje?")
-        break
+        else:
+            print("Halo, halo co tu się dzieje?")
+        continue
