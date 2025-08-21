41d910f693b1fdcb8b42f7198ca6d877972e295b
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-21 16:36:03 +0200

    Slice assigment, shallow copy

---
+++ AIworkflow/Slicing.py
@@ -0,0 +1,16 @@
+print("-----------------------")    # SLICE ASSIGNMENT
+nums = [10,20,30,40,50,60,70]
+nums[1:1] = [1,2,3]                 # Dodawanie dokładnie miedzy index 0 a 1 nowej listy
+print(nums)
+print("-----------------------")
+nums[3:4] = [5,5,5,5]               # Podmień index nr 3 i wstaw listę, przesuwając resztę w prawo
+print(nums)
+print("-----------------------")
+nums[0:7] = []                      # Podmień indexy od 0 do 6! na pustą liste, czyli wykasuj ;P
+print(nums)
+print("-----------------------")
+nums = [10,20,30,40,50,60,70]
+nums2 = nums[:]                     # Płytka kopia(SHALLOW COPY), UWAGA tworzy nową listę, ale zawartość patrzy na starą listę!
+print(nums2)                        # Jeżeli płytka kopia listy zawiera zagnieżdżone elementy typu lista w liscie, słowniki w liście 
+                                    # to operacje na tych zmiennych dotyczą także oryginału, to jest b. ważne!!!
+
