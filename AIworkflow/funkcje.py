b51d06116fe6171dec8d734b49f4f76a7dbaba64
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-26 12:24:07 +0200

    samples

--- AIworkflow/funkcje.py
+++ AIworkflow/funkcje.py
@@ -1,4 +1,37 @@
+# FUNKCJE UŻYTKOWNIKA
+
 def przywitaj_sie():
     print("Hello!")
 
 przywitaj_sie()
+print("----------------------------")
+
+
+def przedstaw_sie(a,b):
+    return f"Cześć {a} , masz {b} lata?!"
+
+x = przedstaw_sie("Robert", 43)
+print(x)
+print("----------------------------")
+
+
+def pole_kwadratu(a):
+    return a*a
+
+x = pole_kwadratu(5)
+print(x)
+print("----------------------------")
+
+
+def czy_parzysta(liczba):
+    return liczba % 2==0
+
+liczba = int(input("Podaj liczbę: "))
+if czy_parzysta(liczba):
+    print("Parzysta")
+else:
+    print("Nieparzysta")
+print("----------------------------")
+
+
+
