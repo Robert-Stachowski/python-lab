2636a87f39b2ccf4178d5b23a45b91ceb3da824f
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-28 11:17:33 +0200

    Cwiczenia funkcje

---
+++ AIworkflow/Funkje_cwiczenia.py
@@ -0,0 +1,41 @@
+from functools import reduce
+
+liczby = [1, 2, 3, 4]
+iloczyn = reduce(lambda a, b: a * b, liczby)
+print(iloczyn)
+print("-----------------------------------")
+
+# 1
+x = int(input("Podaj liczbę: "))
+cube = lambda x: x**3
+print(cube(x))
+print("-----------------------------------")
+
+# 2
+lista = [10,20,30]
+napis = list(map(str, lista))                   # tu zamieniamy listę na string!
+print(napis)
+
+a = [str(x) for x in lista]                     # list comprehension
+print(a)
+print("-----------------------------------")
+
+# 3
+lista_1 = [1,2,3,4,5,6,7,8,9]
+nieparzyste = list(filter(lambda x: x % 2 == 1, lista_1))       # Szukanie liczb NIEPARZYSTYCH
+print(nieparzyste)
+
+comp = [x for x in lista_1 if x % 2 ==1]                        # list comprehension
+print(comp)
+print("-----------------------------------")
+
+# 4
+lista_2 = ["kot", "pies", "zebra", "lew", "slon"]
+ostatnia_litera = sorted(lista_2, key = lambda char: char[-1])  # Sortowanie po ostatniej literze elementu
+print(ostatnia_litera)
+
+
+
+
+
+
