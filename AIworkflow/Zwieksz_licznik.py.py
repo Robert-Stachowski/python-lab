c49e1072b95a3b38402cf9df161a241e2107de25
Robert Stachowski <Robertstachowski0@gmail.com>
2025-10-06 13:02:26 +0200

    Counter

---
+++ AIworkflow/Zwieksz_licznik.py.py
@@ -0,0 +1,12 @@
+class Licznik:
+    def __init__(self,licznik):
+        self.licznik = licznik
+        
+    def zwiększ(self):
+        self.licznik = 1
+        for i in range(3):
+            self.licznik += i
+        return self.licznik
+        
+obj = Licznik(0)
+print(obj.zwiększ())
