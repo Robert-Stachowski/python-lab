99e1ab647ea019b3da27b5fb40996f2800d7280b
Robert Stachowski <Robertstachowski0@gmail.com>
2025-11-27 20:31:40 +0100

    add validation for response.json

--- Weather_CLI/weather_client.py
+++ Weather_CLI/weather_client.py
@@ -1,7 +1,7 @@
 import requests
 
 
-class Weather_client:
+class WeatherClient:
     def __init__(self):
         self.base_url = "https://example_url.com/api"
         self.headers = { "sample_headers" : "sample_headers"}
@@ -10,9 +10,12 @@ class Weather_client:
         
 
     def get_city_weather(self, city:str):
-        if city is None or isinstance(city, str) or not city.strip():
-            raise ValueError("Brak danych")
-        url = "f{self.base_url}/weather?city = {city}" # przykładowo zbudowane url do fejkowego api pogody, wywołującego podane maisto(city)
+        if city is None or not isinstance(city, str) or not city.strip():
+            raise ValueError("Brak danych / Niepoprawne dane")
+        url = f"{self.base_url}/weather?city={city}" # przykładowe URL do fikcyjnego API pogody — city to nazwa miasta użytkownika
         response = self.session.get(url, timeout=5)
         response.raise_for_status()
-        return response.json()
+        data = response.json()
+        if "temp_c" not in data or "condition" not in data:
+            raise ValueError("Brak wymaganych pól")
+        return data
