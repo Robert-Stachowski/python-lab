e3577cd6c539e345ad101306144dcde3807234d0
Robert Stachowski <Robertstachowski0@gmail.com>
2026-01-27 11:17:57 +0100

    add User ORM model with timestamps and server-side defaults

---
+++ SQLAlchemy/step_2_tworzenie_tabeli.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, Text, String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    # user account
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    username = Column(String(255), unique=True, nullable=True)
+    password = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    bio = Column(Text)
+    avatar_url = Column(Text)
+    created_at = Column(DateTime, server_default=func.now()) #server_default ustawia wartość(czas) po stronie bazy danych przy INSERT, 
+    updated_at = Column(DateTime, server_default=func.now()) # więc po commit() w ORM może być None i trzeba zrobić session.refresh(obj).
+
+    def __repr__(self):
+        return f"User: {self.username}"
