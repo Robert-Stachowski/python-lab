025210865e139f0c0b379ec96c0e10ddf43dd6f6
Robert Stachowski <Robertstachowski0@gmail.com>
2026-02-12 08:53:14 +0100

    Add model Category & Product with 1:N realtioship

--- Bazy_danych/cwiczenia/04/models.py
+++ Bazy_danych/cwiczenia/04/models.py
@@ -11,9 +11,10 @@ Base = declarative_base()
 class Category(Base):
     __tablename__ = "categories"
 
-    # Tutaj zdefiniuj kolumny i relacje
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=100), nullable=False, unique=True)
 
+    category_rel = relationship("Product", back_populates="products_rel", cascade="all, delete-orphan")
 
 # TODO: Zdefiniuj model Product
 # Kolumny: id, name, price, quantity, category_id
@@ -21,5 +22,10 @@ class Category(Base):
 class Product(Base):
     __tablename__ = "products"
 
-    # Tutaj zdefiniuj kolumny i relacje
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=100), nullable=False)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer, nullable=False, default=0)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+    products_rel = relationship("Category", back_populates="category_rel")
