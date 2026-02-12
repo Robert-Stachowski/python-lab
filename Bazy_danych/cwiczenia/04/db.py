c53ae34c6544e845c8915709043fd4a36645efc1
Robert Stachowski <Robertstachowski0@gmail.com>
2026-02-12 08:52:21 +0100

    database and session configration added

--- Bazy_danych/cwiczenia/04/db.py
+++ Bazy_danych/cwiczenia/04/db.py
@@ -5,6 +5,6 @@ import os
 
 load_dotenv()
 
-# TODO: Pobierz DATABASE_URL z .env
-# TODO: Utworz engine za pomoca create_engine()
-# TODO: Utworz SessionLocal za pomoca sessionmaker()
+DATABASE_URL=os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(engine)
