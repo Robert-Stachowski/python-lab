a2d091673d5e865f12c46c51265e3d25b77b7b69
Robert Stachowski <Robertstachowski0@gmail.com>
2026-02-23 09:28:28 +0100

    add Room, Guest, Reservation models with relationshop

--- Bazy_danych/cwiczenia/09/models.py
+++ Bazy_danych/cwiczenia/09/models.py
@@ -10,9 +10,13 @@ Base = declarative_base()
 class Room(Base):
     __tablename__ = "rooms"
 
-    # Tutaj zdefiniuj kolumny i relacje
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=100), nullable=False)
+    capacity = Column(Integer, nullable=False)
+    price_per_night = Column(Float, nullable=False)
+    is_available = Column(Boolean, default=True)
 
+    reservations = relationship("Reservation", back_populates="room", cascade="all, delete-orphan")
 
 # TODO: Zdefiniuj model Guest
 # Kolumny: id, name, email (UNIQUE), phone
@@ -20,9 +24,12 @@ class Room(Base):
 class Guest(Base):
     __tablename__ = "guests"
 
-    # Tutaj zdefiniuj kolumny i relacje
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=100), nullable=False)
+    email = Column(String(length=100), nullable=False, unique=True)
+    phone = Column(String(length=20))
 
+    reservations = relationship("Reservation", back_populates="guest", cascade="all, delete-orphan")
 
 # TODO: Zdefiniuj model Reservation
 # Kolumny: id, guest_id (FK), room_id (FK), check_in, check_out, total_price, status
@@ -30,5 +37,13 @@ class Guest(Base):
 class Reservation(Base):
     __tablename__ = "reservations"
 
-    # Tutaj zdefiniuj kolumny i relacje
-    pass
+    id = Column(Integer, primary_key=True)
+    guest_id = Column(Integer, ForeignKey("guests.id"))
+    room_id = Column(Integer, ForeignKey("rooms.id"))
+    check_in = Column(Date, nullable=False)
+    check_out = Column(Date, nullable=False)
+    total_price = Column(Float, nullable=False)
+    status = Column(String(length=20), default="potwierdzona")
+
+    room = relationship("Room", back_populates="reservations")
+    guest = relationship("Guest", back_populates="reservations")
