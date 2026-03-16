0dabf3ff02b3c25d02a76dacba082482a42d8836
Robert Stachowski <Robertstachowski0@gmail.com>
2026-03-16 12:16:31 +0100

    export all routers from routers package

---
+++ Projekty/Task_Manager_API/app/routers/__init__.py
@@ -0,0 +1,5 @@
+from .users import router as users_router
+from .tasks import router as tasks_router
+from .tags import router as tags_router
+from .stats import router as stats_router
+from .projects import router as projects_router
