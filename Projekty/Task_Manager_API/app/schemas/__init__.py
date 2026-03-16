226dac33846c27c8c2537c2c2901d6a9e0274eab
Robert Stachowski <Robertstachowski0@gmail.com>
2026-03-16 12:05:56 +0100

    export all Pydantic schemas from schemas package

--- Projekty/Task_Manager_API/app/schemas/__init__.py
+++ Projekty/Task_Manager_API/app/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .user import UserCreate, UserResponse, UserUpdate
 from .tag import TagCreate, TagResponse
 from .project import ProjectCreate, ProjectUpdate, ProjectResponse, ProjectWithTasksResponse
-from .task import TaskStatus, TaskPriority, TaskCreate, TaskResponse, TaskUpdate, TaskStatusUpdate
+from .task import TaskStatus, TaskPriority, TaskCreate, TaskResponse, TaskUpdate, TaskStatusUpdate 
