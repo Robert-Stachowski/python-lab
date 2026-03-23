5c35b9b1cea9266226ff2097cdc75e5ef9c27211
Robert Stachowski <Robertstachowski0@gmail.com>
2026-03-23 12:55:23 +0100

    add ValueError handler to os.getenv

--- Projekty/Task_Manager_API/app/config.py
+++ Projekty/Task_Manager_API/app/config.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("Brak lub błędy klucz dostępu")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
