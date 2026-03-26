c4885a7316b7ab371f9d8b8d8157b5ed0f1be073
Robert Stachowski <Robertstachowski0@gmail.com>
2026-03-26 13:06:06 +0100

    Add test_delete_task and secure task mutation endpoints in tests, Dodano test_delete_task sprawdzający usunięcie taska i odpowiedź 404, zabezpieczono testy PUT i DELETE tokenem JWT zgodnie z ochroną endpintów

--- Projekty/Task_Manager_API/tests/test_tasks.py
+++ Projekty/Task_Manager_API/tests/test_tasks.py
@@ -156,3 +156,28 @@ def test_filter_tasks_by_priority(client, test_project, test_user, auth_token):
     tasks = task_priority_filter.json()
     assert len(tasks["items"]) == 1
     assert tasks["items"][0]["priority"] == "medium"
+
+
+
+def test_delete_task(client, test_user, test_task, test_project, auth_token):
+    assert test_user["username"] == "test_data_username"
+    assert test_user["email"] == "example@exapmle.pl"
+
+    
+    assert test_project["name"] == "Test projekt numer jeden. "
+    assert test_project["description"] == "Opis projektu. "
+    assert test_project["owner_id"] == test_user["id"]
+    
+    project_id = test_project["id"]
+
+
+    assert test_task["title"] == "Przykładowy tytuł zadania"
+    assert test_task["description"] == "Przykładowy opis zadania"
+    assert test_task["project_id"] == project_id
+
+
+    delete_task = client.delete(f"/tasks/{test_task['id']}", headers={"Authorization": f"Bearer {auth_token}"})
+    assert delete_task.status_code == 204
+
+    find_task = client.get(f"/tasks/{test_task['id']}")
+    assert find_task.status_code == 404
